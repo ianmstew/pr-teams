f776a240a94972ab0d6d65b07ff2d220c343c365
Ian Stewart <ian.stewart@nuix.com>
2016-07-24 23:47:26 -0400

    Vastly imporoved algorithm--weighted to add reviewers from same team

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var bodyParser = require('body-parser');
 var prTeams = require('./lib/pr-teams');
 var app = express();
 
+var TEXT = 'text/plain';
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 var USAGE =
-  'Usage: <PR team size>:<developer team>[;<developer team>]*\n' +
-  '  where <developer team> = <developer>[,<developer>]*;\b' +
-  'Example: 4:brett,stef,steve;ian,max;zach,josh';
+  'Usage: <PR team size>:<Min outsiders>:<developer team>[;<developer team>]*\n' +
+  '  where <developer team> = <developer>[,<developer>]*\n' +
+  'Example: 4:1:brett,stef,steve;ian,max;zach,josh';
 
 
 app.post('/generate-teams', function (req, res) {
@@ -22,20 +24,35 @@ app.post('/generate-teams', function (req, res) {
   var text = req.body.text.trim();
 
   var textSplit = text.split(':');
-  if (textSplit.length !== 2) {
-    return res.send(USAGE);
+  if (textSplit.length !== 3) {
+    return res.type(TEXT).send(USAGE);
   }
 
   var prTeamSize = parseInt(textSplit[0], 10);
   if (Number.isNaN(prTeamSize)) {
-    return res.send(USAGE);
+    return res.type(TEXT).send(USAGE);
+  }
+  if (prTeamSize < 1) {
+    return res.type(TEXT).send('PR team size must be at least 1.');
   }
 
-  var devTeams = textSplit[1].split(';').map(function (devTeamStr) {
+  var minOutsiders = parseInt(textSplit[1], 10);
+  if (Number.isNaN(minOutsiders)) {
+    return res.type(TEXT).send(USAGE);
+  }
+  if (minOutsiders < 0) {
+    return res.type(TEXT).send('Min outsiders must be at least 0.');
+  }
+
+  if (minOutsiders > prTeamSize) {
+    return res.type(TEXT).send('Min outsiders must be no more than PR team size.');
+  }
+
+  var devTeams = textSplit[2].split(';').map(function (devTeamStr) {
     return devTeamStr.split(',');
   });
 
-  res.send(prTeams.generatePrTeams(devTeams, prTeamSize));
+  res.send(prTeams.generatePrTeams(devTeams, prTeamSize, minOutsiders));
 });
 
 app.listen(app.get('port'), function () {
